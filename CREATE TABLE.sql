b3677b33eaaa5c904f741f62ba915c6a0adeee48
whLee <rnsk1243@gmail.com>
2021-03-28 22:56:45 +0900

    branch:StockWork ＃====絵文字==== ＃👍：+1：機能の改善 ＃✨：sparkles：追加の部分機能 ＃====フォーマット==== ＃：emoji：件名#issue No. ＃ ＃コミットボディ...  Creon APIからChartデータ取得  Chart区分別に取得可能 ＃==== 7つのルール==== ＃1。件名と本文を空白行で区切ります ＃2。件名を50文字に制限します ＃3。件名を大文字にします ＃4。件名をピリオドで終わらせないでください ＃5。件名に命令法を使用する ＃6。体を72文字で包む ＃7。本文を使用して、何を、なぜ、どのように説明するか

---
+++ CREATE TABLE.sql
@@ -0,0 +1,99 @@
+CREATE TABLE `daily_price_month` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(20) NOT NULL,
+	`date` DATETIME NULL DEFAULT NULL,
+	`open` BIGINT(20) NULL DEFAULT NULL,
+	`high` BIGINT(20) NULL DEFAULT NULL,
+	`low` BIGINT(20) NULL DEFAULT NULL,
+	`close` BIGINT(20) NULL DEFAULT NULL,
+	`diff` BIGINT(20) NULL DEFAULT NULL,
+	`volume` BIGINT(20) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`, `code`),
+	UNIQUE INDEX `date` (`date`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+
+----------------------------------------
+
+CREATE TABLE `daily_price_week` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(20) NOT NULL,
+	`DATE` DATETIME NULL DEFAULT NULL,
+	`week` SMALLINT(6) NULL DEFAULT NULL,
+	`open` BIGINT(20) NULL DEFAULT NULL,
+	`high` BIGINT(20) NULL DEFAULT NULL,
+	`low` BIGINT(20) NULL DEFAULT NULL,
+	`close` BIGINT(20) NULL DEFAULT NULL,
+	`diff` BIGINT(20) NULL DEFAULT NULL,
+	`volume` BIGINT(20) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`, `code`),
+	UNIQUE INDEX `DATE` (`DATE`, `week`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+----------------------------------------
+
+CREATE TABLE `daily_price_day` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(20) NOT NULL,
+	`date` DATETIME NOT NULL,
+	`week` SMALLINT(6) NULL DEFAULT NULL,
+	`open` BIGINT(20) NULL DEFAULT NULL,
+	`high` BIGINT(20) NULL DEFAULT NULL,
+	`low` BIGINT(20) NULL DEFAULT NULL,
+	`close` BIGINT(20) NULL DEFAULT NULL,
+	`diff` BIGINT(20) NULL DEFAULT NULL,
+	`volume` BIGINT(20) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`, `code`),
+	UNIQUE INDEX `date` (`date`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+ 
+----------------------------------------
+
+CREATE TABLE `daily_price_min` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(20) NOT NULL,
+	`dailyCount` BIGINT(20) NOT NULL,
+	`DATE` DATETIME NOT NULL,
+	`week` SMALLINT(6) NULL DEFAULT NULL,
+	`open` BIGINT(20) NULL DEFAULT NULL,
+	`high` BIGINT(20) NULL DEFAULT NULL,
+	`low` BIGINT(20) NULL DEFAULT NULL,
+	`close` BIGINT(20) NULL DEFAULT NULL,
+	`diff` BIGINT(20) NULL DEFAULT NULL,
+	`volume` BIGINT(20) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`, `code`),
+	UNIQUE INDEX `dailyCount` (`dailyCount`, `DATE`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+----------------------------------------
+
+CREATE TABLE `daily_price_tick` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(20) NOT NULL,
+	`dailyCount` BIGINT(20) NULL DEFAULT NULL,
+	`DATE` DATETIME NULL DEFAULT NULL,
+	`week` SMALLINT(6) NULL DEFAULT NULL,
+	`open` BIGINT(20) NULL DEFAULT NULL,
+	`high` BIGINT(20) NULL DEFAULT NULL,
+	`low` BIGINT(20) NULL DEFAULT NULL,
+	`close` BIGINT(20) NULL DEFAULT NULL,
+	`diff` BIGINT(20) NULL DEFAULT NULL,
+	`volume` BIGINT(20) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`, `code`),
+	UNIQUE INDEX `dailyCount` (`dailyCount`, `DATE`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
--- Creon/Creon.py
+++ Creon/Creon.py
@@ -1,6 +1,6 @@
 from pywinauto import application
 import time
-import os,json
+import os, json
 import ctypes
 import win32com.client
 from logging import config, getLogger
@@ -10,6 +10,7 @@ LT_TRADE_REQUEST = 0
 LT_NONTRADE_REQUEST = 1
 LT_SUBSCRIBE = 2
 MAX_REQUEST_NUM = 2221
+
 
 class Creon:
     def __init__(self):
@@ -37,7 +38,6 @@ class Creon:
         except Exception as e:
             self.__logger.error(f"Exception occured Creon init : {str(e)}")
 
-
     def LoginCreon(self):
         os.system('taskkill /IM coStarter* /F /T')
         os.system('taskkill /IM CpStart* /F /T')
@@ -54,7 +54,6 @@ class Creon:
         """CREONPLUSEシステムつながりチェックする"""
         # 管理者権限で実行したのか
         if not ctypes.windll.shell32.IsUserAnAdmin():
-
             self.__logger.info('check_creon_system() : admin user -> FAILED')
             return False
 
@@ -94,9 +93,9 @@ class Creon:
             df['date'] = df['date'].astype(str)
             df['date'] = df['date'].str.cat(df['time'], sep=' ')
             df['date'] = pd.to_datetime(df['date'], format='%Y%m%d %H%M')
-            df['weekday'] = df['date'].apply(f_weekday)
+            df['week'] = df['date'].apply(f_weekday)
             df['diff'] = df['close'].diff(-1).fillna(0).astype(int)
-            df = df[['dailyCount', 'date', 'weekday', 'open', 'high', 'low', 'close', 'diff', 'volume']]
+            df = df[['dailyCount', 'date', 'week', 'open', 'high', 'low', 'close', 'diff', 'volume']]
             return df
 
         elif chartType == "m":
@@ -105,16 +104,16 @@ class Creon:
             df['date'] = df['date'].astype(str)
             df['date'] = df['date'].str.cat(df['time'], sep=' ')
             df['date'] = pd.to_datetime(df['date'], format='%Y%m%d %H%M')
-            df['weekday'] = df['date'].apply(f_weekday)
+            df['week'] = df['date'].apply(f_weekday)
             df['diff'] = df['close'].diff(-1).fillna(0).astype(int)
-            df = df[['dailyCount', 'date', 'weekday', 'open', 'high', 'low', 'close', 'diff', 'volume']]
+            df = df[['dailyCount', 'date', 'week', 'open', 'high', 'low', 'close', 'diff', 'volume']]
             return df
 
         elif chartType == "D":
 
             df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
-            df['weekday'] = df['date'].apply(f_weekday)
-            df = df[['date', 'weekday', 'open', 'high', 'low', 'close', 'diff', 'volume']]
+            df['week'] = df['date'].apply(f_weekday)
+            df = df[['date', 'week', 'open', 'high', 'low', 'close', 'diff', 'volume']]
             return df
 
         elif chartType == "W":
@@ -138,7 +137,7 @@ class Creon:
             return None
 
     # Chart情報取得
-    def RequestChartAmount(self, code, chartType = None, requestAmount = None):
+    def RequestChartAmount(self, code, chartType=None, requestAmount=None):
         """
         株価のChart情報取得を行う。行う際に最近順で取得する。
         :param code: 株式コード
@@ -146,17 +145,15 @@ class Creon:
         :param requestAmount: 取得数（Noneの場合、最後まで取得）
         :return: pandas.DataFrame
         """
-
-        dailyCountList = []  #一つの種類に対して1日分の株株価の数番号(１番号がスタートの株価)
-        dailyCountList2 = [] #要請数の通りに取得した株価の数番号
-        stockDateList = []   #日付
-        stockTimeList = []   #時間
-        stockOpenList = []   #Open
-        stockHighList = []   #High
-        stockLowList = []    #Low
-        stockCloseList = []  #Close
-        stockDiffList = []   #Diff
-        stockVolumeList = [] #Volume
+        dailyCountList = []  # 臨時に集めた番号
+        stockDateList = []  # 日付
+        stockTimeList = []  # 時間
+        stockOpenList = []  # Open
+        stockHighList = []  # High
+        stockLowList = []  # Low
+        stockCloseList = []  # Close
+        stockDiffList = []  # Diff
+        stockVolumeList = []  # Volume
         dailyCount = 0
 
         if chartType is None:
@@ -211,83 +208,143 @@ class Creon:
         # 取引量区分
         self.__objStockChart.SetInputValue(pType7, ord(pValue7))
 
-        requestedAmount = 0 #受信データ数累計
-        while pValue2 > requestedAmount: #要請数が受信データ累計より大きい場合、受信繰り返す。
-            self.__CheckandWait(LT_NONTRADE_REQUEST) #要請可能か？チェック
-            self.__objStockChart.BlockRequest() #受信したデータ以降のデータを要請する。
-            # rqStatus = self.__objStockChart.GetDibStatus()
-            # rqRet = self.__objStockChart.GetDibMsg1()
-            # print("통신상태", rqStatus, rqRet)
-            # if rqStatus != 0:
-            #     exit()
+        requestedAmount = 0  # 受信データ数累計
+        result_DWM = pd.DataFrame()
+        while pValue2 > requestedAmount:  # 要請数が受信データ累計より大きい場合、受信繰り返す。
+            self.__CheckandWait(LT_NONTRADE_REQUEST)  # 要請可能か？チェック
+            self.__objStockChart.BlockRequest()  # 受信したデータ以降のデータを要請する。
 
             curRequestedAmount = self.__objStockChart.GetHeaderValue(3)  # 受信した数
             self.__logger.info(f"受信数: {curRequestedAmount}")
             requestedAmount += curRequestedAmount
             oldDate = self.__objStockChart.GetDataValue(0, 0)
-            tempList = []
-            isNextDate = False
+
             for i in range(curRequestedAmount):
                 if pValue4 == 'T' or pValue4 == 'm':
                     newDate = self.__objStockChart.GetDataValue(0, i)
                     if oldDate == newDate:
                         dailyCount += 1
-                    else:
+                        dailyCountList.append(dailyCount)
+                        oldDate = self.__objStockChart.GetDataValue(0, i)
+
+                        stockDateList.append(self.__objStockChart.GetDataValue(0, i))
+                        stockTimeList.append(self.__objStockChart.GetDataValue(1, i))
+                        stockOpenList.append(self.__objStockChart.GetDataValue(2, i))
+                        stockHighList.append(self.__objStockChart.GetDataValue(3, i))
+                        stockLowList.append(self.__objStockChart.GetDataValue(4, i))
+                        stockCloseList.append(self.__objStockChart.GetDataValue(5, i))
+                        stockDiffList.append(self.__objStockChart.GetDataValue(6, i))
+                        stockVolumeList.append(self.__objStockChart.GetDataValue(7, i))
+
+                    if oldDate != newDate or curRequestedAmount < MAX_REQUEST_NUM:
+
+                        stockDateList = list(reversed(stockDateList))
+                        stockTimeList = list(reversed(stockTimeList))
+                        stockOpenList = list(reversed(stockOpenList))
+                        stockHighList = list(reversed(stockHighList))
+                        stockLowList = list(reversed(stockLowList))
+                        stockCloseList = list(reversed(stockCloseList))
+                        stockDiffList = list(reversed(stockDiffList))
+                        stockVolumeList = list(reversed(stockVolumeList))
+
+                        result = pd.DataFrame({'dailyCount': dailyCountList,
+                                               'date': stockDateList,
+                                               'time': stockTimeList,
+                                               'open': stockOpenList,
+                                               'high': stockHighList,
+                                               'low': stockLowList,
+                                               'close': stockCloseList,
+                                               'diff': stockDiffList,
+                                               'volume': stockVolumeList,
+                                               }, index=[i for i in range(1, dailyCount + 1)])
+
                         oldDate = newDate
-                        dailyCount = 1
-                        isNextDate = True
-                    tempList.append(dailyCount)
+                        result_Tm = self.__transformDataFrameDB(result, chartType)
+                        for row in result_Tm.itertuples(name='count'):
+                            self.__logger.info(row)
 
-                stockDateList.append(self.__objStockChart.GetDataValue(0, i))
-                stockTimeList.append(self.__objStockChart.GetDataValue(1, i))
-                stockOpenList.append(self.__objStockChart.GetDataValue(2, i))
-                stockHighList.append(self.__objStockChart.GetDataValue(3, i))
-                stockLowList.append(self.__objStockChart.GetDataValue(4, i))
-                stockCloseList.append(self.__objStockChart.GetDataValue(5, i))
-                stockDiffList.append(self.__objStockChart.GetDataValue(6, i))
-                stockVolumeList.append(self.__objStockChart.GetDataValue(7, i))
-            #end for
+                        # ------------DB INSERT--------------
 
-            tempList = list(reversed(tempList))  # リスト要素逆順にする。1,2,3,...,99,..
-            if isNextDate is False:
-                dailyCountList = tempList + dailyCountList  # リスト結合
-            else:
-                #日替りの場合
-                tempList1 = tempList[:tempList.index(1) + 1]
-                tempList2 = tempList[tempList.index(1) + 1:]
-                dailyCountList = tempList2 + dailyCountList  # リスト結合
-                dailyCountList2 = dailyCountList2 + dailyCountList
-                dailyCountList = tempList1
+                        # ------------DB INSERT--------------
+
+                        dailyCountList = []
+                        stockDateList = []  # 日付
+                        stockTimeList = []  # 時間
+                        stockOpenList = []  # Open
+                        stockHighList = []  # High
+                        stockLowList = []  # Low
+                        stockCloseList = []  # Close
+                        stockDiffList = []  # Diff
+                        stockVolumeList = []  # Volume
+                        dailyCount = 0
+
+                else: # Chart区分がT,m以外の場合
+                    dailyCount += 1
+                    dailyCountList.append(dailyCount)
+                    stockDateList.append(self.__objStockChart.GetDataValue(0, i))
+                    stockTimeList.append(self.__objStockChart.GetDataValue(1, i))
+                    stockOpenList.append(self.__objStockChart.GetDataValue(2, i))
+                    stockHighList.append(self.__objStockChart.GetDataValue(3, i))
+                    stockLowList.append(self.__objStockChart.GetDataValue(4, i))
+                    stockCloseList.append(self.__objStockChart.GetDataValue(5, i))
+                    stockDiffList.append(self.__objStockChart.GetDataValue(6, i))
+                    stockVolumeList.append(self.__objStockChart.GetDataValue(7, i))
+
+            # --------------------------end for----------------------------------------
+            if pValue4 != 'T' and pValue4 != 'm':
+                stockDateList = list(reversed(stockDateList))
+                stockTimeList = list(reversed(stockTimeList))
+                stockOpenList = list(reversed(stockOpenList))
+                stockHighList = list(reversed(stockHighList))
+                stockLowList = list(reversed(stockLowList))
+                stockCloseList = list(reversed(stockCloseList))
+                stockDiffList = list(reversed(stockDiffList))
+                stockVolumeList = list(reversed(stockVolumeList))
+
+                result = pd.DataFrame({'dailyCount': dailyCountList,
+                                       'date': stockDateList,
+                                       'time': stockTimeList,
+                                       'open': stockOpenList,
+                                       'high': stockHighList,
+                                       'low': stockLowList,
+                                       'close': stockCloseList,
+                                       'diff': stockDiffList ,
+                                       'volume': stockVolumeList,
+                                       }, index=[i for i in range(1, dailyCount + 1)])
+
+                result_DWM = self.__transformDataFrameDB(result, chartType).append(result_DWM)
+
+                dailyCountList = []
+                stockDateList = []  # 日付
+                stockTimeList = []  # 時間
+                stockOpenList = []  # Open
+                stockHighList = []  # High
+                stockLowList = []  # Low
+                stockCloseList = []  # Close
+                stockDiffList = []  # Diff
+                stockVolumeList = []  # Volume
+                dailyCount = 0
 
             if curRequestedAmount < MAX_REQUEST_NUM:
-                #取得した結果がMAX_REQUEST_NUMより小さかったら目標まで取得したとみなしてwhile抜ける
-                break #end while
+                self.__logger.info('Creonにある全データを取得した。')
+                self.__logger.info(f"code : {code} / 取得したデータ数 : {requestedAmount}")
 
-        if pValue4 == 'T' or pValue4 == 'm':
-            dailyCountList2 = dailyCountList2 + dailyCountList
-            result = pd.DataFrame({'dailyCount': dailyCountList2,
-                                   'date': stockDateList,
-                                   'time': stockTimeList,
-                                   'open': stockOpenList,
-                                   'high': stockHighList,
-                                   'low': stockLowList,
-                                   'close': stockCloseList,
-                                   'diff': stockDiffList,
-                                   'volume': stockVolumeList,
-                                   }, index=[i for i in range(1, requestedAmount + 1)])
-        else:
-            result = pd.DataFrame({'date': stockDateList,
-                                   'time': stockTimeList,
-                                   'open': stockOpenList,
-                                   'high': stockHighList,
-                                   'low': stockLowList,
-                                   'close': stockCloseList,
-                                   'diff': stockDiffList,
-                                   'volume': stockVolumeList,
-                                   }, index=[i for i in range(1, requestedAmount + 1)])
+                # 取得した結果がMAX_REQUEST_NUMより小さかったら目標まで取得したとみなしてwhile抜ける
+                break  # end
 
-        self.__logger.info(f"code : {code} / 取得したデータ数 : {requestedAmount}")
-        return self.__transformDataFrameDB(result, chartType)
+        if pValue4 != 'T' and pValue4 != 'm':
+
+            result_DWM['Index'] = [i for i in range(1, requestedAmount + 1)]
+            result_DWM = result_DWM.set_index('Index')
+
+            for row in result_DWM.itertuples(name='count'):
+                self.__logger.info(row)
+
+            # ------------DB INSERT--------------
+
+            # ------------DB INSERT--------------
+
+        return
 
     def SearchStockInfo(self):
         """
@@ -303,22 +360,22 @@ class Creon:
         lenStockInfo = len(stockList1) + len(stockList2)
 
         for i, code in enumerate(stockList1):
-            #secondCode = self.__objCpCodeMgr.GetStockSectionKind(code) # 副 区分コード
-            #stdPrice = self.__objCpCodeMgr.GetStockStdPrice(code)      # 基準価額
-            name = self.__objCpCodeMgr.CodeToName(code) # 株名称
+            # secondCode = self.__objCpCodeMgr.GetStockSectionKind(code) # 副 区分コード
+            # stdPrice = self.__objCpCodeMgr.GetStockStdPrice(code)      # 基準価額
+            name = self.__objCpCodeMgr.CodeToName(code)  # 株名称
             stockCodeList.append(code)
             stockNameList.append(name)
             self.__logger.info(f"code : {code} // company : {name}")
 
         for i, code in enumerate(stockList2):
-            #secondCode = self.__objCpCodeMgr.GetStockSectionKind(code) # 副 区分コード
-            #stdPrice = self.__objCpCodeMgr.GetStockStdPrice(code)      # 基準価額
-            name = self.__objCpCodeMgr.CodeToName(code) # 株名称
+            # secondCode = self.__objCpCodeMgr.GetStockSectionKind(code) # 副 区分コード
+            # stdPrice = self.__objCpCodeMgr.GetStockStdPrice(code)      # 基準価額
+            name = self.__objCpCodeMgr.CodeToName(code)  # 株名称
             stockCodeList.append(code)
             stockNameList.append(name)
             self.__logger.info(f"code : {code} // company : {name}")
 
-        dfStockInfo = pd.DataFrame({'code':stockCodeList,
-                                    'company':stockNameList},
-                                   index=[i for i in range(1,lenStockInfo+1)])
+        dfStockInfo = pd.DataFrame({'code': stockCodeList,
+                                    'company': stockNameList},
+                                   index=[i for i in range(1, lenStockInfo + 1)])
         return dfStockInfo
